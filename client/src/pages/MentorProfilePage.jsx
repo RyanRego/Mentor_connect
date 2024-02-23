56fbde96f6398f89b03d06f94cb445702483aa42
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2024-02-23 08:39:37 +0530

    mentor profile page change

--- client/src/pages/MentorProfilePage.jsx
+++ client/src/pages/MentorProfilePage.jsx
@@ -1,90 +1,206 @@
-
 import { useEffect, useState } from "react";
-import ProfilePhoto from "../assets/mentor.png";
-import Carousal from "../components/testimonial/Carousal";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
-const MentorProfilePage = () => {
+import DateTimePicker from "react-datetime-picker";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+import {
+  IconClockHour2,
+  IconStarFilled,
+  IconCurrencyRupee,
+  IconVideo,
+  IconMessage,
+  IconSquareCheck,
+} from "@tabler/icons-react";
+
+const MentorProfilePage = () => {
   const [mentor, setMentor] = useState({});
   const user = useSelector((state) => state.user);
   const getMentor = async () => {
     try {
-      axios.post("/data/mentor/get-mentor-profile-data", {
-        email: user?.currentUser?.email,
-      }).then((res) => {
-        console.log(res.data);
-        setMentor(res.data);
-      });
+      axios
+        .post("/data/mentor/get-mentor-profile-data", {
+          email: user?.currentUser?.mentor?.email,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setMentor(res.data);
+        });
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(() => {
     getMentor();
-  },[]);
+  }, []);
+
+  const [value, onChange] = useState(new Date());
+  console.log(value);
 
   return (
-    <div className="pb-28 md:px-16 pt-4 h-[95vh] overflow-auto text-gray-700">
-      <div className="w-full h-44 bg-gray-300 rounded-lg mb-9 shadow-sm"></div>
-      <div className="flex flex-col md:flex-row gap-8 items-center justify-between">
-        <div className="flex gap-6 items-center">
-          <img
-            src={mentor.profilePicture ||ProfilePhoto}
-            className="h-32 w-32 rounded-full"
-            alt="profile"
-          />
-          <div className="flex flex-col gap-2">
-            <h1 className="text-xl font-semibold">{mentor?.firstName} {mentor?.lastName}</h1>
-            <h3 className="text-sm">{mentor.currentlyWorkingAt}</h3>
-            <div className="flex gap-4">
-              <span className="text-sm">⭐ 4.9</span>
-              <span className="text-sm text-green-600">🟢 20+ mentees</span>
+    <div className="pt-24 p-3 md:p-12 h-[100vh] overflow-auto text-gray-700">
+      <div className="flex flex-col md:flex-row gap-6">
+        <div className="md:w-2/3">
+          <div className="bg-gray-50 w-full border border-gray-200 rounded-lg shadow-sm">
+            <div className="relative h-40 bg-blue-200 rounded-t-lg w-full mb-12">
+              <div className="absolute left-10 -bottom-10">
+                <img
+                  src={mentor.profilePicture}
+                  className="h-32 w-32 rounded-full p-1 bg-white"
+                  alt="profile"
+                />
+              </div>
+            </div>
+            <div className="pb-6 flex flex-col md:flex-row gap-8 items-center justify-between">
+              <div className="flex gap-6 items-center px-6">
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-2xl font-semibold">
+                    {mentor?.firstName} {mentor?.lastName}
+                  </h1>
+                  <h3 className="">{mentor.currentlyWorkingAt}</h3>
+                  <div className="flex gap-6">
+                    <span className="flex items-center text-sm">
+                      <IconStarFilled
+                        className="mr-2 text-yellow-600"
+                        size={16}
+                      />
+                      5
+                    </span>
+                    <span className="flex items-center text-sm">
+                      <IconClockHour2 className="mr-2" size={16} />
+                      1800+ Mentoring mins
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-gray-50 mt-6 rounded-lg border border-gray-200 p-6 w-full shadow-sm">
+            <h1 className="text-xl font-semibold">About</h1>
+            <p className="mt-2 text-sm">{mentor?.bio}</p>
+
+            <h1 className="mt-[72px] text-xl font-semibold">Expertise areas</h1>
+            <div className="flex gap-4 flex-wrap mt-2 text-sm">
+              {mentor?.proficiency?.map((item) => (
+                <div
+                  key={item}
+                  className="bg-white flex items-center font-semibold px-2 py-1 rounded-lg border border-gray-300 shadow-sm"
+                >
+                  {item}
+                </div>
+              ))}
             </div>
           </div>
         </div>
-        <div className="p-4 mt-9 md:mt-0 bg-white shadow-sm border border-green-400 rounded-lg">
-          <div className="flex items-center gap-4">
-            <span className="text-3xl font-bold">${mentor.rateOfMentorship}</span>
-            <button className="px-4 h-9 bg-green-600 rounded-lg text-white font-semibold">
+        <div className="md:w-1/3 flex flex-col gap-6">
+          <div className="p-6 flex flex-col justify-between gap-4 bg-green-50 border border-gray-200 shadow-sm rounded-lg">
+            <div>
+              <p className="flex text-sm items-center">
+                <IconVideo className="mr-4" />
+                one to one vedio call
+              </p>
+              <p className="flex text-sm items-center">
+                <IconSquareCheck className="mr-4" />
+                get resources to follow
+              </p>
+              <p className="flex text-sm items-center">
+                <IconMessage className="mr-4" />
+                chat with mentor
+              </p>
+            </div>
+            <span className="flex items-center text-2xl font-semibold">
+              <IconCurrencyRupee />
+              {mentor.rateOfMentorship}
+              <span className="text-base font-normal">/hr</span>
+            </span>
+
+            <div className="flex justify-center flex-col">
+              <p className="font-semibold mb-2 text-sm">Pick a time slot</p>
+              <DateTimePicker
+                className="bg-white"
+                onChange={onChange}
+                value={value}
+              />
+            </div>
+
+            <button className="px-4 h-9 shadow-sm bg-green-700 rounded-lg text-white font-semibold">
               Book a session
             </button>
           </div>
-          <p className="mt-4">Average response time 1hr</p>
+
+          <div>
+            <h1 className="mb-2 font-semibold">Demo video</h1>
+            <iframe
+              className="w-full"
+              src={`https://www.youtube.com/embed/0PfiXof0E9s`}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+            />
+          </div>
         </div>
       </div>
 
-      <div className="mt-16 rounded-lg">
-        <h1 className="text-2xl font-semibold underline decoration-blue-300">
-          Description
-        </h1>
-        <p className="mt-2">
-          {mentor?.bio}
-        </p>
-      </div>
-      <div className="mt-9 rounded-lg ">
-        <h1 className="text-2xl font-semibold underline decoration-blue-300">
-          Proficiency:{" "}
-        </h1>
-        <div className="flex gap-4 flex-wrap mt-4">
-          {mentor?.proficiency?.map((item) => (
-            <div
-              key={item}
-              className="flex items-center px-4 h-9 rounded-full bg-gray-300"
-            >
-              {item}
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="mt-28">
-        <h1 className="font-semibold text-3xl mb-9 underline decoration-blue-300">
-          Happy Mentees
+      <div className="mt-20 md:flex flex-col max-w-[540px]">
+        <h1 className="text-center mb-6 text-xl font-semibold">
+          Happy mentees 😄
         </h1>
         <div>
-          <Carousal />
+          <Swiper navigation={true} modules={[Navigation]}>
+            <SwiperSlide>
+              <div className="p-6 mx-12 md:mx-16 bg-yellow-50 rounded-lg flex flex-col gap-2 items-center">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png"
+                  alt=""
+                  className="h-14 w-14"
+                />
+                <p className="font-semibold text-sm">Manoj</p>
+                <span className="flex gap-1">
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                </span>
+
+                <p className="text-center text-sm mt-2">
+                  Interacting with Yash was delightful. He took time to
+                  understand my issue in a few sessions without rushing into
+                  solutions.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="p-6 mx-12 md:mx-16 bg-yellow-50 rounded-lg flex flex-col gap-2 items-center">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png"
+                  alt=""
+                  className="h-14 w-14"
+                />
+                <p className="font-semibold text-sm">Manoj</p>
+                <span className="flex gap-1">
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                  <IconStarFilled className="mr-2 text-yellow-600" size={16} />
+                </span>
+
+                <p className="text-center text-sm mt-2">
+                  Interacting with Yash was delightful. He took time to
+                  understand my issue in a few sessions without rushing into
+                  solutions.
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
